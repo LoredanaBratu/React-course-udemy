c7622bda0cb756185a76c6d1beffb1cfcebc471b
Loredana <loredana_bratu@yahoo.com>
2020-04-27 17:24:37 +0300

    display content conditionally with if statment in render method

--- src/App.js
+++ src/App.js
@@ -14,23 +14,6 @@ class App extends Component {
     showPersons: true,
   };
 
-  // const app = (props) => {
-  //   const [personsState, setPersonsState] = useState({
-  //     persons: [
-  //       { name: "Maximilian", age: 30 },
-  //       { name: "Manu", age: 20 },
-  //     ],
-  //   });
-
-  // const handleSwitchName = () => {
-  //   // this.setState({
-  //   setPersonsState({
-  //     persons: [
-  //       { name: "Emilian", age: 20 },
-  //       { name: "Thomas", age: 30 }
-  //     ]
-  //   });
-  // };
   handleSwitchName = (newName) => {
     this.setState({
       persons: [
@@ -50,8 +33,9 @@ class App extends Component {
   };
 
   handleTogglePersons = () => {
+    const doesShow = this.state.showPersons;
     this.setState({
-      showPersons: !this.state.showPersons,
+      showPersons: !doesShow,
     });
   };
 
@@ -63,38 +47,56 @@ class App extends Component {
       background: "white",
       border: "1px solid black",
     };
+
+    const { persons, showPersons } = this.state;
+
+    let personsItems = null;
+    if (showPersons) {
+      personsItems = (
+        <div>
+          <Person
+            name={persons[0].name}
+            age={persons[0].age}
+            click={this.handleSwitchName}
+            handleChange={this.handleInputChange}
+          />
+          <Person
+            name={persons[1].name}
+            age={persons[1].age}
+            click={this.handleSwitchName.bind(this, "Paul")}
+          >
+            My bobby: football
+          </Person>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <button style={style} onClick={this.handleTogglePersons}>
           Hide content
         </button>
         <h1>I'm a react App</h1>
-        {this.state.showPersons ? (
+        {personsItems}
+        {/* {showPersons ? (
           <div>
             <Person
-              // name={this.state.persons[0].name}
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
+              name={persons[0].name}
+              age={persons[0].age}
               click={this.handleSwitchName}
               handleChange={this.handleInputChange}
             />
             <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
+              name={persons[1].name}
+              age={persons[1].age}
               click={this.handleSwitchName.bind(this, "Paul")}
             >
               My bobby: football
             </Person>
           </div>
-        ) : null}
+        ) : null} */}
       </div>
     );
-    // return React.createElemen(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement("h1", "Im a new element")
-    // ); //da error
   }
 }
 export default App;
-//export default app;
